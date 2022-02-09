127e73930463738d269f0575b7a80a77cd7a5bf5
pedrohfonseca <48777052+pedrohfonseca@users.noreply.github.com>
2022-02-09 19:36:21 -0300

    Add files via upload

---
+++ Semana08/calculadora/calculadora.js
@@ -0,0 +1,15 @@
+var valor;
+var resultado;
+
+function botao(num){
+    valor = document.calc.visor.value += num;
+}
+
+function reset(){
+    document.calc.visor = '';
+}
+
+function calcula(){
+    resultado = eval(valor)
+    document.calc.visor.value = resultado.toLocaleString('pt-br');
+}
